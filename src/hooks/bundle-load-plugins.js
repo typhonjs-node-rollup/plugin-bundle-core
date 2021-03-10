b80ee51a4f184296c38ecc3e6c2431068bc50588
typhonrt <support@typhonrt.org>
2021-03-09 21:09:16 -0800

    pass only necessary options to plugins loaded

--- src/hooks/bundle-load-plugins.js
+++ src/hooks/bundle-load-plugins.js
@@ -47,7 +47,8 @@ export default async function(options)
       {
          loaded += `- ${loader.packageName}\n`;
 
-         global.$$pluginManager.add({ name: loader.packageName, instance: loader, options });
+         global.$$pluginManager.add({ name: loader.packageName, instance: loader,
+          options: { id: options.id, flags: options.flags } });
       }
 
       if (loaded !== '')
